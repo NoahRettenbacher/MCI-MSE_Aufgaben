2832231b5daa98170d77d152eead78956cf6010b
NoahRettenbacher <you@example.com>
2022-03-28 21:45:02 +0200

    update 2.3

--- Aufgaben_PÜ/PÜ2/Abgabe 2/main_1_comments_only.py
+++ Aufgaben_PÜ/PÜ2/Abgabe 2/main_1_comments_only.py
@@ -1,4 +1,33 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
+    # Eingangsordner erstellen 
+    # Ergometerdaten mit richtigem Datentyp in einem Vektor speichern
+    # Vektoren in Eingangsordner speichern
+    
+#%% UC 2.2 Vorverarbeitung der Daten
+    # Nur Messpunkte innerhalb der Testzeit werden berücksichtig 
+    # Nicht relevante Daten wie z.B. stark von den anderen abweichende Daten werden aussortiert. 
+    # Verwendete Daten mit passenden Signalfiltern bereinigt.
+#%% UC 2.3 Analysieren der Daten auf Abbruchkriterium
+    # Prüfen der tatsächlichen Testzeit 
+    # t < 180 s -> möglicher Abbruch nicht alle Daten erfasst 
+#%% UC 2.4 Erstellen der Zusammenfassung
+    # Daten statistisch auswerten mittels geeigneter Funktion
+    # min/max avg  Herzfrequenz/Puls/Sauerstoffsättigung
+#%% UC 2.5 Visualisierung der Daten 
+    # Je Messparameter wird ein Plot mit line-chart erstellt 
+    # Herzfrequenz
+    # Puls
+    # Sauerstoffsättigung
+#%% UC 2.6 Manuelle Eingabe eines Abbruch-Kriteriums
+    # Mittels pop up Fenster kann manueller Abbruch getätig werden 
+    # Speichern der Zeit bei Abbruch
+    # Gemessene Daten trotzdem in separatem Ordner gespeichert
+#%% UC 2.7 Speichern der Daten 
+    # Daten, Visuallisierungen, erfolgreich abgeschlossene Tests und Abgebrochene Tests 
+    # werden in verschiedenen Ordnern gespeichert 
+    
 
+
+#%%
