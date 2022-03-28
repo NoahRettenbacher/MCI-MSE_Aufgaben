811a4560fd344600f225ea510d068b2fc4d0f55e
NoahRettenbacher <you@example.com>
2022-03-28 21:48:10 +0200

    Update Aufgabe 2.3

--- Aufgaben_PÜ/PÜ2/Abgabe 2/main_1_comments_only.py
+++ Aufgaben_PÜ/PÜ2/Abgabe 2/main_1_comments_only.py
@@ -9,21 +9,26 @@
     # Nur Messpunkte innerhalb der Testzeit werden berücksichtig 
     # Nicht relevante Daten wie z.B. stark von den anderen abweichende Daten werden aussortiert. 
     # Verwendete Daten mit passenden Signalfiltern bereinigt.
+
 #%% UC 2.3 Analysieren der Daten auf Abbruchkriterium
     # Prüfen der tatsächlichen Testzeit 
     # t < 180 s -> möglicher Abbruch nicht alle Daten erfasst 
+
 #%% UC 2.4 Erstellen der Zusammenfassung
     # Daten statistisch auswerten mittels geeigneter Funktion
     # min/max avg  Herzfrequenz/Puls/Sauerstoffsättigung
+
 #%% UC 2.5 Visualisierung der Daten 
     # Je Messparameter wird ein Plot mit line-chart erstellt 
     # Herzfrequenz
     # Puls
     # Sauerstoffsättigung
+
 #%% UC 2.6 Manuelle Eingabe eines Abbruch-Kriteriums
     # Mittels pop up Fenster kann manueller Abbruch getätig werden 
     # Speichern der Zeit bei Abbruch
     # Gemessene Daten trotzdem in separatem Ordner gespeichert
+    
 #%% UC 2.7 Speichern der Daten 
     # Daten, Visuallisierungen, erfolgreich abgeschlossene Tests und Abgebrochene Tests 
     # werden in verschiedenen Ordnern gespeichert 
