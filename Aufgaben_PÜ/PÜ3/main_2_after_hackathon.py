6de64c374b45494ecfa7b69bbbe1bfef7bbfbd24
bergdude <ej4427@mci4me.at>
2022-04-02 17:43:47 +0200

    comments

--- Aufgaben_PÜ/PÜ3/main_2_after_hackathon.py
+++ Aufgaben_PÜ/PÜ3/main_2_after_hackathon.py
@@ -1,9 +1,19 @@
 # UC 2.0 
 #methoden mit docstrings versehen --> daraus soll objekt erstellt werden
 
+# Kommentare Johannes (mit "J" davor)
+# J Zwischen den Unteraufgaben sind Absätze hinzugefügt worden zur besseren Übersicht
+
+
+
+
 #%% UC 2.1 Einlesen der Daten
 # Musterlösung 
-#Funktion für einlesen der Daten 
+# Funktion für einlesen der Daten 
+
+# J Funktion erstellen: Eingabe = folder_input_data; Ausgabe = list_of_new_tests
+# Funktion überprüft, ob im übergegebenen Ordner Dateien enthalten sind und
+# gibt eine Liste mit den Filenamen zurück
 
 list_of_new_tests = []
 ## Überprüfen ob Dateien vorhanden sind
@@ -27,12 +37,23 @@ for file in os.listdir(folder_input_data):
 
 new_ecg_data["Subject_3"].plot()
 
+
+
+
+
+
+
+
+
 #%% UC 2.2 Vorverarbeiten der Daten
 
 ## Anlegen einer Zeitreihe der Herzfrequenz aus den EKG-Daten 
 
-#TESTMETHODEn find peaks () findminimum ()--> oop Testklasse erstellen plot in testklasse
+# TESTMETHODEn find peaks () findminimum ()--> oop Testklasse erstellen plot in testklasse
 
+# J Funktion erstellen, die eine Klasse zurückgibt -> geht das ? 
+# Methoden: plot HR Moving Average
+# Eigenschaften: number of heartbeats, duration test, average heartrate, heartrate moving average
 
 import neurokit2 as nk
 
@@ -54,7 +75,21 @@ peaks['average_HR_10s'] = peaks.rolling(window=10000).mean()*60*1000
 peaks['average_HR_10s'].plot()
 
 
+
+
+
+
+
+
+
+
+
 #%% UC 2.3 Analysieren der Daten auf Abbruch-Kriterium
+
+# J 
+# Funktion def pruefen_abbruch()
+# Output = termination (false = kein abbruch, true = abbruch)
+# Input = file_name ?
 
 termination = False
 
@@ -69,7 +104,7 @@ import json
 file_name = folder_input_data = os.path.join(folder_input_data, 'subject_3.json')
 
 f = open(file_name)
- 
+
 # returns JSON object as
 # a dictionary
 subject_data = json.load(f)
@@ -82,7 +117,19 @@ subject_max_hr = 220 - (2022 - subject_data["birth_year"])
 if maximum_hr > subject_max_hr*0.90:
     termination = True
 
+
+
+
+
+
+
+
+
+
 #%% UC 2.4 Erstellen einer Zusammenfassung
+
+# J Funktion erstellen, die eine Zusammenfassung ausgibt
+# Input: file_name oder subject_data oder folder_input_data
 
 print("Summary for Subject " + str(subject_data["subject_id"]))
 print("Year of birth:  " + str(subject_data["birth_year"]))
@@ -92,6 +139,17 @@ print("Maximum HR was: " + str(maximum_hr))
 print("Was test terminated because exceeding HR " + str(termination)) #Printsummary Proband als Subjekt pro person daten ausgegeben
 
 ## Ausgabe einer Zusammenfassung
+
+
+
+
+
+
+
+
+
+
+
 
 #%% UC 2.5 Visualisierung der Daten
 
@@ -124,15 +182,34 @@ peaks_downsampled.plot()
 
 #peaks_downsampled["Power (Watt)"].plot()
 
+
+
+
+
+
+
+
+
+
+
+
 #%% UC 2.6 Manuelle Eingabe eines Abbruchkritierums
 
 ## Abfrage an Nutzer:in, ob Abgebrochen werden soll
+## Funktion ohne Input & Output "def abfrage_abbruch()"
 
 manual_termination = False
 manual_termination = input("Is this test invalid? (leave blank if valid): ")
 
 if manual_termination != False:
     termination = True
+
+
+
+
+
+
+
 
 
 #%% UC 2.7 Speichern der Daten
